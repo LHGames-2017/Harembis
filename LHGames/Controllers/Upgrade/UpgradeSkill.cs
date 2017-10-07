f0be604c2f63de58721ecff14eb1cde1e3e40e23
Charles-Étienne Joseph <cejoseph@Sansfil-Securise-Etudiants-Lassonde-245-221.polymtl.ca>
2017-10-07 17:10:56 -0400

    Upgrade.cs + UpgradeSkill.cs completed

--- LHGames/Controllers/Upgrade/UpgradeSkill.cs
+++ LHGames/Controllers/Upgrade/UpgradeSkill.cs
@@ -1,4 +1,4 @@
-using System;
+﻿using System;
 using System.Collections.Generic;
 using StarterProject.Web.Api;
 
@@ -7,72 +7,128 @@ namespace Harembis
 
     public class UpgradeSkill : Upgrade
     {
+        public UpgradeSkill(Player ourPlayer, ExtendedInfo infos) : base(ourPlayer, infos){}
 
-        UpgradeTypes nextUpgrade_;
+		public Tuple<UpgradeType, int, int> getNextSkill(){
+            UpgradeType type;
+            int cost, weight;
 
-
-
-        uint attackWeight_;
-
-        uint protectionWeight_;
-
-        uint gatherWeight_;
-        UpgradeTypes nextGather;
-
-
-
-
-   Upgrade(Player ourPlayer)
-        {
-            ourPlayer_ = ourPlayer;
-            updateNext();
-
-        }
-
-        public Tuple<UpgradeTypes, uint> getNext() { return new Tuple<UpgradeTypes, uint>(nextUpgrade_, cost_); }
-
-        public void updateNext()
-        {
-            Upgrade.updateNext();
-            if (attackWeight_ > protectionWeight_ && attackWeight_ > gatherWeight_)
-            {
-                nextUpgrade_
+            if (attackWeight_ > defenceWeight_ && attackWeight_ > healthWeight_ && attackWeight_ > capacityWeight_ && attackWeight_ > collectingWeight_){
+                type = UpgradeType.AttackPower;
+                cost = infos_.nextUpgradeCost(UpgradeType.AttackPower);
+                weight = attackWeight_;
+            }
+            else if(defenceWeight_ > healthWeight_ && defenceWeight_ > capacityWeight_ && defenceWeight_ > collectingWeight_){
+				type = UpgradeType.Defence;
+				cost = infos_.nextUpgradeCost(UpgradeType.Defence);
+				weight = defenceWeight_;
+            }
+            else if(healthWeight_ > capacityWeight_ && healthWeight_ > collectingWeight_) {
+                type = UpgradeType.MaximumHealth;
+                cost = infos_.nextUpgradeCost(UpgradeType.MaximumHealth);
+				weight = healthWeight_;
+            }
+            else if(capacityWeight_ > collectingWeight_) {
+                type = UpgradeType.CarryingCapacity;
+                cost = infos_.nextUpgradeCost(UpgradeType.CarryingCapacity);
+				weight = capacityWeight_;
+            }
+            else {
+                type = UpgradeType.CollectingSpeed;
+                cost = infos_.nextUpgradeCost(UpgradeType.CollectingSpeed);
+				weight = collectingWeight_;
             }
 
+            return new Tuple<UpgradeType, int, int>(type, cost, weight);
         }
 
-        void updateAllWeight() { }
+        protected override void updateAttack(){
+            attackWeight_ = Constantes.ATTACK_MODIFIER * calculateWeight(2, infos_.getAttack(), infos_.nextUpgradeCost(UpgradeType.AttackPower));
+        }
 
+        protected override void updateCollecting(){
+            double bonus;
 
-
-        void updateAttackDefenceWeight(uint totalAttackDefence, uint attackDefenceLevel)
-        {
-
-            uint prix;
-
-            switch (attackLevel)
-            {
-                case 0:
-                    prix = 15000;
+            switch (infos_.getLevels().CollectingSpeed){
+                case 0 :
                 case 1:
-                    prix = 40000;
+                    bonus = 0.25;
+                    break;
                 case 2:
-                    prix = 100000;
                 case 3:
-                    prix = 250000;
+                    bonus = 0.5;
+                    break;
                 case 4:
-                    prix = 500000;
-
+                    bonus = 1;
+                    break;
+                default:
+                    bonus = 0;
+                    break;
 
             }
 
-            attackWeight = (2 / (totalAttack * prix));
+            collectingWeight_ = Constantes.GATHER_MODIFIER * calculateWeight(bonus, infos_.getGatherSpeed(), infos_.nextUpgradeCost(UpgradeType.CollectingSpeed));
+		}
 
+        protected override void updateDefence(){
+            defenceWeight_ = Constantes.DEFENCE_MODIFIER * calculateWeight(2, infos_.getDefence(), infos_.nextUpgradeCost(UpgradeType.Defence));
 
+		}
+
+        protected override void updateHealth(){
+			double bonus;
+
+            switch (infos_.getLevels().MaximumHealth)
+			{
+                case 0:
+                    bonus = 3;
+                    break;
+                case 1 :
+                    bonus = 2;
+                    break;
+                case 2:
+                case 3:
+                    bonus = 5;
+                    break;
+                case  4:
+                    bonus = 10;
+                    break;
+                default:
+                    bonus = 0;
+                    break;
+			}
+
+            healthWeight_ = Constantes.HEALTH_MODIFIER * calculateWeight(bonus, ourPlayer_.MaxHealth, infos_.nextUpgradeCost(UpgradeType.MaximumHealth));
+        }
+
+        protected override void updateCapacity(){
+			double bonus;
+
+            switch (infos_.getLevels().CarryingCapacity)
+			{
+                case 0 :
+                    bonus = 500;
+                    break;
+                case 1 :
+                    bonus = 1000;
+                    break;
+                case 2 :
+                    bonus = 2500;
+                    break;
+                case 3 :
+                    bonus = 5000;
+                    break;
+                case 4 :
+                    bonus = 15000;
+                    break;
+                default:
+                    bonus = 0;
+                    break;
+
+			}
+
+            capacityWeight_ = Constantes.CAPACITE_MODIFIER * calculateWeight(bonus, ourPlayer_.CarryingCapacity, infos_.nextUpgradeCost(UpgradeType.CarryingCapacity));
         }
 
     }
-
-}
-
 }
